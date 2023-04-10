c331211d0a1c0f430c8b26b902c458a82a154632
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2023-04-10 11:20:01 +0900

    Make the ui Apple like

--- pages/lp.tsx
+++ pages/lp.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Typography, Box, Button, Grid, Paper } from "@mui/material";
+import { Container, Typography, Box, Button, Grid } from "@mui/material";
 import { useRouter } from "next/router";
 import { Chat, School, BusinessCenter, Language } from "@mui/icons-material";
 
@@ -11,7 +11,13 @@ const HomePage: React.FC = () => {
   };
 
   return (
-    <Box sx={{ backgroundColor: "#f5f5f5", minHeight: "100vh" }}>
+    <Box
+      sx={{
+        backgroundColor: "#F2F2F7",
+        minHeight: "100vh",
+        fontFamily: "Inter, sans-serif",
+      }}
+    >
       <Container maxWidth="md">
         <Box
           sx={{
@@ -24,87 +30,121 @@ const HomePage: React.FC = () => {
             gap: 3,
           }}
         >
-          <Typography variant="h2" component="h1" color="primary">
+          <Typography
+            variant="h2"
+            component="h1"
+            sx={{ fontWeight: 600, color: "#1F1F1F" }}
+          >
             Unlock Your English Potential with AI
           </Typography>
-          <Typography variant="h5" component="h2" color="secondary">
+          <Typography
+            variant="h5"
+            component="h2"
+            sx={{ fontWeight: 500, color: "#1F1F1F" }}
+          >
             AIとともに英語の可能性を解き放とう
           </Typography>
-          <Typography variant="subtitle1" component="p">
+          <Typography
+            variant="subtitle1"
+            component="p"
+            sx={{ fontWeight: 400, color: "#1F1F1F" }}
+          >
             AIがあなたの英語学習をサポート！最先端の技術で効果的な学習体験を提供します。
           </Typography>
           <Grid container spacing={2} justifyContent="center" mt={4}>
             <Grid item xs={12} sm={6} md={3}>
-              <Paper
-                elevation={3}
+              <Box
                 sx={{
                   display: "flex",
                   flexDirection: "column",
                   alignItems: "center",
                   justifyContent: "center",
-                  height: 200,
+                  minHeight: 200,
                   padding: 2,
                 }}
               >
-                <Chat color="primary" style={{ fontSize: 60 }} />
-                <Typography variant="h6">リアルタイム対話</Typography>
-              </Paper>
+                <Chat style={{ fontSize: 60, color: "#1F1F1F" }} />
+                <Typography
+                  variant="h6"
+                  sx={{ fontWeight: 500, color: "#1F1F1F" }}
+                >
+                  リアルタイム対話
+                </Typography>
+              </Box>
             </Grid>
             <Grid item xs={12} sm={6} md={3}>
-              <Paper
-                elevation={3}
+              <Box
                 sx={{
                   display: "flex",
                   flexDirection: "column",
                   alignItems: "center",
                   justifyContent: "center",
-                  height: 200,
+                  minHeight: 200,
                   padding: 2,
                 }}
               >
-                <School color="primary" style={{ fontSize: 60 }} />
-                <Typography variant="h6">効果的な学習</Typography>
-              </Paper>
+                <School style={{ fontSize: 60, color: "#1F1F1F" }} />
+                <Typography
+                  variant="h6"
+                  sx={{ fontWeight: 500, color: "#1F1F1F" }}
+                >
+                  効果的な学習
+                </Typography>
+              </Box>
             </Grid>
             <Grid item xs={12} sm={6} md={3}>
-              <Paper
-                elevation={3}
+              <Box
                 sx={{
                   display: "flex",
                   flexDirection: "column",
                   alignItems: "center",
                   justifyContent: "center",
-                  height: 200,
+                  minHeight: 200,
                   padding: 2,
                 }}
               >
-                <BusinessCenter color="primary" style={{ fontSize: 60 }} />
-                <Typography variant="h6">ビジネス英語対応</Typography>
-              </Paper>
+                <BusinessCenter style={{ fontSize: 60, color: "#1F1F1F" }} />
+                <Typography
+                  variant="h6"
+                  sx={{ fontWeight: 500, color: "#1F1F1F" }}
+                >
+                  ビジネス英語対応
+                </Typography>
+              </Box>
             </Grid>
             <Grid item xs={12} sm={6} md={3}>
-              <Paper
-                elevation={3}
+              <Box
                 sx={{
                   display: "flex",
                   flexDirection: "column",
                   alignItems: "center",
                   justifyContent: "center",
-                  height: 200,
+                  minHeight: 200,
                   padding: 2,
                 }}
               >
-                <Language color="primary" style={{ fontSize: 60 }} />
-                <Typography variant="h6">多言語サポート</Typography>
-              </Paper>
+                <Language style={{ fontSize: 60, color: "#1F1F1F" }} />
+                <Typography
+                  variant="h6"
+                  sx={{ fontWeight: 500, color: "#1F1F1F" }}
+                >
+                  多言語サポート
+                </Typography>
+              </Box>
             </Grid>
           </Grid>
           <Button
             variant="contained"
-            color="primary"
+            sx={{
+              backgroundColor: "#007AFF",
+              color: "#FFFFFF",
+              mt: 4,
+              "&:hover": {
+                backgroundColor: "#005FD7",
+              },
+            }}
             size="large"
             onClick={handleButtonClick}
-            sx={{ mt: 4 }}
           >
             Start Your Free Trial
           </Button>
